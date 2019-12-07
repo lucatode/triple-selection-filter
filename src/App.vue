0addd063812f2123933220dffc2dd39261a239c2
lucatode <l.todeschini88@gmail.com>
2019-12-07 02:50:37 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <v-content>
+            <v-container>
+                <v-layout row>
+                    <v-autocomplete
+                            :items="getItems.map(x=>x.language)"
+                            v-model="language"
+                    ></v-autocomplete>
+                    <v-autocomplete
+                            :items="getItems
+                                      .filter(x=>x['language']===language)
+                                      .map(x=>x.file)"
+                            v-model="file"
+                    ></v-autocomplete>
+                    <v-autocomplete
+                            :items="getItems
+                                      .filter(x=>x['language']===language)
+                                      .filter(x=>x['file']===file)
+                                      .map(x=>x.version)"
+                            v-model="version"
+                    ></v-autocomplete>
+                </v-layout>
+                <v-layout row>
+                    {{getText(language, file, version)}}
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import {mapActions, mapGetters} from "vuex";
+
+    export default {
+        name: 'App',
+
+        components: {},
+
+        data: () => ({
+            //
+            language: {},
+            file: {},
+            version: {}
+        }),
+
+        computed: {
+            ...mapGetters(['getItems'])
+        },
+
+        methods: {
+            ...mapActions(['loadItems']),
+            getText(language, file, version) {
+                let item = this.getItems
+                    .filter(x => x['language'] === language)
+                    .filter(x => x['file'] === file)
+                    .filter(x => x['version'] === version);
+                return item.length === 1 ? item[0].text : ''
+            }
+        },
+
+        created() {
+            this.loadItems();
+        }
+
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
